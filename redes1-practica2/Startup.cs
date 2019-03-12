c41843c353b68a712f5b3911b61ede6a5636b0e2
ctejaxun <cesar.etx@gmail.com>
2019-03-11 22:11:12 -0600

    agrega defeniciones ef core  y modelos

--- redes1-practica2/Startup.cs
+++ redes1-practica2/Startup.cs
@@ -4,8 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace redes1_practica2
 {
@@ -22,6 +26,22 @@ namespace redes1_practica2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            var conn = "Server=192.168.76.134;Database=redes1_practica2;User=root;";
+            if ("Development" == Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"))
+            {
+                conn = "Server=192.168.0.199;Database=redes1_practica2;User=root;";
+            }
+            // other service configurations go here
+            services.AddDbContextPool<VehiculosDB>( // replace "YourDbContext" with the class name of your DbContext
+                options => options.UseMySql(conn, // replace with your Connection String
+                    mySqlOptions =>
+                    {
+                        mySqlOptions.ServerVersion(new Version(5, 7, 17), ServerType.MySql); // replace with your Server Version and Type
+                    }
+            ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ redes1-practica2/VehiculosDB.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace redes1_practica2
+{
+    public class VehiculosDB : DbContext
+    {
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+
+        public VehiculosDB(DbContextOptions<VehiculosDB> options) : base(options)
+        {
+
+        }
+    }
+
+    public class Vehiculo
+    {
+        [Key]
+        public int IDVehiculo { get; set; }
+        public string Nombre { get; set; }
+    }
+}
