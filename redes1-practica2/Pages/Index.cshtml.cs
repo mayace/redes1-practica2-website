f8c60c583b32cf2cd0ca4ad7d5e8484d0ed9f86e
ctejaxun <cesar.etx@gmail.com>
2019-03-11 22:10:08 -0600

    actualiza index y vehiculos con ef

--- redes1-practica2/Pages/Index.cshtml.cs
+++ redes1-practica2/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,19 @@ namespace redes1_practica2.Pages
 {
     public class IndexModel : PageModel
     {
+
+        private IHttpContextAccessor _accessor = null;
+        public IndexModel(IHttpContextAccessor accessor)
+        {
+            this._accessor = accessor;
+        }
+
+        public String IPAddress { get; set; }
+        public Boolean EsServidorVentas { get; set; }
         public void OnGet()
         {
-
+            this.IPAddress = this._accessor.HttpContext.Request.Host.Value;
+            this.EsServidorVentas = this._accessor.HttpContext.Request.Host.Value == "192.168.76.68";
         }
     }
 }
---
+++ redes1-practica2/Pages/Vehiculos.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace redes1_practica2.Pages
+{
+    public class VehiculosModel : PageModel
+    {
+
+        public IEnumerable<Vehiculo> Vehiculos { set; get; }
+
+        VehiculosDB db = null;
+        public VehiculosModel(VehiculosDB db)
+        {
+            this.db = db;
+        }
+
+        [BindProperty]
+        public Vehiculo vehiculo { get; set; }
+
+        public void OnGet()
+        {
+            this.Vehiculos = this.db.Vehiculos.ToList();
+        }
+
+        //public void OnPost()
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        this.db.Vehiculos.Add(this.vehiculo);
+        //        this.db.SaveChanges();
+        //    }
+        //}
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            this.db.Vehiculos.Add(this.vehiculo);
+            await this.db.SaveChangesAsync();
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var found = await db.Vehiculos.FindAsync(id);
+            var res = id;
+
+            if (found != null)
+            {
+                db.Vehiculos.Remove(found);
+                res = await db.SaveChangesAsync();
+            }
+
+            return RedirectToPage(new { data = res });
+        }
+    }
+}
